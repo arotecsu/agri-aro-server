75aa46bef4e68f7642ef5f3cfdf93ae4a13d0876
Sebastião Sukuakueche <sukuakueches@gmail.com>
2026-03-08 22:26:13 +0100

    fix: error on render

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -3,11 +3,15 @@ import { jwtService } from "../auth/jwt";
 
 export interface AuthRequest extends Request {
   userId: string;
+  params: Record<string, string>;
+  query: Record<string, string | string[]>;
+  body: any;
+  headers: Record<string, string | string[] | undefined>;
 }
 
 class AuthMiddleware {
   async use(req: AuthRequest, res: Response, next: NextFunction) {
-    const authHeader = req.headers.authorization;
+    const authHeader = req.headers.authorization as string | undefined;
     const token = authHeader ? authHeader.split(" ")[1] : "";
 
     if (!token) return res.sendStatus(401);
